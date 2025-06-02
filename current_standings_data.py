aa2749578b189a07c547f734018ad0140056ba61
RyanAbbottData <ryanabbott855@gmail.com>
2025-06-01 22:49:29 -0500

    updates to fix bug

---
+++ current_standings_data.py
@@ -0,0 +1,32 @@
+import datetime
+import sys
+
+import statsapi
+import pandas as pd
+
+
+sys.path.append(r"C:\Users\ryana\portfolio")
+
+from utils.sql import write_to_sql
+
+
+standings_for_today = statsapi.standings_data(leagueId="103,104", division="all", include_wildcard=True, season=None, standingsTypes=None, date=None)
+
+
+
+div_keys = list(standings_for_today.keys())
+
+new_dict = {}
+
+for key in div_keys:
+    new_dict[standings_for_today[key]['div_name']] = standings_for_today[key]['teams']
+
+standings_for_df = []
+
+for key, value in new_dict.items():
+    for e in new_dict[key]:
+        e['division'] = key
+        e['time'] = datetime.datetime.now()
+        standings_for_df.append(e)
+
+write_to_sql(standings_for_df, 'mlb', 'daily_standings')
